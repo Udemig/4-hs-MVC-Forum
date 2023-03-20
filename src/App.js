d6897a70e7a4ea70ef44bb0ecbb066449a1903e6
furkanevin <furkanevin00@gmail.com>
2023-03-21 00:29:01 +0300

    final project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddPostController from './pages/AddPost/AddPostController';
+import ListPostsController from './pages/ListPosts/ListPostsController';
+import './styles/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListPostsController />} />
+        <Route path="/add-post" element={<AddPostController />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
